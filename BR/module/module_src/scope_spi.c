14f525d16686be76be5f70b07811dabb05395d9a
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2024-09-13 21:49:38 +0200

    Added simple SPI example

--- BR/module/module_src/scope_spi.c
+++ BR/module/module_src/scope_spi.c
@@ -1,25 +1,57 @@
+#include "linux/of.h"
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/spi/spi.h>
 
-/* Meta Information */
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ProgramistaPrzemyslaw");
 MODULE_DESCRIPTION("VHDLscope module");
 
-/**
- * @brief This function is called, when the module is loaded into the kernel
- */
-static int __init my_init(void) {
-	printk("Hello World!\n");
+#define BUS_NUM_SPI 0
+static struct spi_device *VHDLscope;
+
+static int __init initModule(void) {
+	u8 msg[] = {0b00001000, 0b00000000, 0b00000100};
+	struct spi_master *master;
+	struct spi_board_info spartan_fpga_info={
+		.modalias = "spartan-3a",
+		.max_speed_hz = 10000000,
+		.bus_num = BUS_NUM_SPI,
+		.chip_select = 0,
+		.mode = 0,
+	};
+	pr_info("Loading module\n");
+	master = spi_busnum_to_master(BUS_NUM_SPI);
+	if(!master){
+		pr_err("No spi bus with Nr %d. Exiting",BUS_NUM_SPI);
+		return -1;
+	}
+
+	VHDLscope = spi_new_device(master, &spartan_fpga_info);
+	if(!VHDLscope){
+		pr_err("Failed to create device \n");
+		return -1;
+	}
+	
+	if(spi_setup(VHDLscope) != 0){
+		pr_err("Could not change bus setup!\n");
+		spi_unregister_device(VHDLscope);
+		return -1;
+	}
+
+	if(spi_write(VHDLscope, msg, sizeof(8))==0){
+		pr_info("MSG send success\n");
+	}else{
+		pr_err("MSG send fail\n");
+	}
+	pr_info("Module initialized\n");
 	return 0;
 }
 
-/**
- * @brief This function is called, when the module is removed from the kernel
- */
-static void __exit my_exit(void) {
+static void __exit exitModule(void) {
 	printk("Goodbye World\n");
 }
 
-module_init(my_init);
-module_exit(my_exit);
+module_init(initModule);
+module_exit(exitModule);

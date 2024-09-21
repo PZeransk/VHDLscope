ab6367ed14574a6f9910cb345e28026b06b5a983
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2024-09-21 12:22:46 +0200

    Started work on linux app

---
+++ BR/module/app_src/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+int main(int argc, char* argv[]){
+    fprintf(stdout,"Hello World \n");
+    return 0;
+}

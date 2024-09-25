d97047e0bb45c12e2bd2fec1e3b36bb63df7bf82
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2024-09-25 23:02:50 +0200

    Fixed config

--- BR/module/external.mk
+++ BR/module/external.mk
@@ -1,2 +1,2 @@
 include $(BR2_EXTERNAL_VHDLSCOPE_PATH)/package/vhdlscope_module/vhdlscope_module.mk
-#include $(sort $(wildcard $(BR2_EXTERNAL_VHDL_SCOPE_PATH)/package/VHDLscope_module/VHDLscope_module.mk))
+include $(BR2_EXTERNAL_VHDLSCOPE_PATH)/package/vhdlscope_app/vhdlscope_app.mk

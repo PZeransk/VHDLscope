0b88827abe8772e035ca44c27606016003d708c3
PZeransk <przemyslaw.zeranski@gmail.com>
2024-10-12 19:37:43 +0200

    Stared work on I2C Controller

---
+++ VHDLscope/src/i2c_controller.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:37:32 10/12/2024 
+-- Design Name: 
+-- Module Name:    i2c_controller - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity i2c_controller is
+	port (
+		
+	);
+end i2c_controller;
+
+architecture Behavioral of i2c_controller is
+
+begin
+
+
+end Behavioral;
+
